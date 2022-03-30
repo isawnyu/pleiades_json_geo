75dbbf9768be1265b507aaa6e5cabb60624c62ef
Tom Elliott <ipse@paregorios.org>
2022-03-30 12:26:18 -0500

    add dependency on Marrow's uri package

--- setup.py
+++ setup.py
@@ -1,24 +1,33 @@
+#
+# This file is part of "pleiades_json_geo"
+# by Tom Elliott for the Institute for the Study of the Ancient World
+# (c) Copyright 2022 by New York University
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="{distribution-name}",
+    name="pleiades_json_geo",
     version="0.0.1",
-    author="{author}",
-    author_email="{author-email}",
-    description="{short-description}",
-    license='MIT',
+    author="Tom Elliot",
+    author_email="tom.elliott@nyu.edu",
+    description="Prepare Pleiades JSON export data for use in QGIS.",
+    license="AGPL-3.0",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    {project-url}
+    url="https://github.com/isawnyu/pleiades_json_geo",
     packages=setuptools.find_packages(),
     classifiers=[
-        "Programming Language :: Python :: {pyver}",
-        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3.10.3",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
         "Operating System :: OS Independent",
     ],
-    install_requires=['airtight'],
-    python_requires='>={pyver}'
+    install_requires=[
+        "airtight",
+        "uri @ git+https://github.com/marrow/uri.git@5b58db87451ca4680004a8993a56bfc4dafff4d4",
+    ],
+    python_requires=">=3.10.3",
 )
