3dfdbb988e35f42490b108932bd2c953d7869bb4
Tom Elliott <ipse@paregorios.org>
2022-03-30 12:18:06 -0500

    stub

---
+++ pleiades_json_geo/pleiades.py
@@ -0,0 +1,9 @@
+#
+# This file is part of "moontask"
+# by Tom Elliott
+# (c) Copyright 2022 by Tom Elliott
+# Licensed under the AGPL-3.0; see LICENSE.txt file.
+#
+"""
+Python 3 package template (changeme)
+"""
